9bc51e35187344c0940e5acb98c87ebd17c581e0
newmohib <@@mohib100@@>
2024-11-01 16:46:36 +0600

    all is working for coffees module with crud

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,9 +11,11 @@ import {
   Query,
   Res,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) {}
 
 //   @Get()
 //   getAll(@Res() response) {
@@ -24,29 +26,34 @@ export class CoffeesController {
 @Get()
   getAll(@Query() paginationQuery) {
     const {limit, offset}= paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}, Offset: ${offset}`;
+    // return `This action returns all coffees. Limit: ${limit}, Offset: ${offset}`;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   getOne(@Param() param) {
-    return `This action returns #${param.id} coffee`;
+    // return `This action returns #${param.id} coffee`;
+    return this.coffeesService.findOne(param.id);
   }
 
   @Post()
   @HttpCode(HttpStatus.OK)
   create(@Body() body) {
-    return body;
+    // return body;
+    return this.coffeesService.create(body);
   }
 
 // Patch is used for partial updates
   @Patch(':id')
   update(@Param('id') id:string, @Body() body) {
-    return `This action updates #${id}`;
+    // return `This action updates #${id}`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id:string) {
-    return `This action removes #${id}`;
+    // return `This action removes #${id}`;
+    return this.coffeesService.remove(id);
   }
    
 }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+    private coffees: Coffee[] = [{
+        id: 1,
+        name: 'Shipwreck Roast',
+        brand: 'Buddy Brew',
+        flavors: ['chocolate', 'vanilla']
+    }]
+
+    // findAll(): Coffee[] {
+    findAll() {
+        return this.coffees;
+    }
+
+    // find one coffee
+    findOne(id: string) {
+        const coffee = this.coffees.find(item => item.id === +id);
+        if (!coffee) {
+            throw new Error(`Coffee #${id} not found`);
+        }
+        return coffee;
+    }
+
+    // create coffee
+    create(createCoffeeDto: any) {
+        this.coffees.push(createCoffeeDto);
+    }
+
+    // update coffee
+    update(id: string, updateCoffeeDto: any) {
+        const existingCoffee = this.findOne(id);
+        this.coffees = this.coffees.map(item => {
+            if (item.id === +id) {
+                return {...existingCoffee, ...updateCoffeeDto};
+            }
+            return item;
+        });
+    }
+
+    // delete coffee
+    remove(id: string) {
+        this.findOne(id);
+        this.coffees = this.coffees.filter(item => item.id !== +id);
+    }
+}
