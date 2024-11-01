6540c8c9ff714ce71ff9ad9b7034fcad95b87aba
newmohib <@@mohib100@@>
2024-11-01 20:41:27 +0600

    added entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity('coffee') // sql table === coffee
 export class Coffee {
+    @PrimaryColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', { nullable: true })
     flavors: string[];
 }
